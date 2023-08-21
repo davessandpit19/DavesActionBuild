8816ee42079585a0d55bf1dca0d69c4eb463a48e
Dave Meek <DMEEK@premierbankcard.com>
2023-08-21 13:46:00 -0500

    first app

---
+++ TestingGithubActions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGithubActions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("look out!");
+            Console.ReadLine();
+        }
+    }
+}
